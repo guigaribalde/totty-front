7a47dc2eafab6b3e9b70ba8579f95794bd58e695
guigaribalde <guilherme@creatusdev.com>
2022-06-22 11:11:36 -0300

    feat: added icon padding and focus style

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,19 @@
-import { Flex } from '@chakra-ui/react'
+import {
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react'
 import {
   ChangeEvent,
   ChangeEventHandler,
   MouseEvent,
   MouseEventHandler,
+  useState,
 } from 'react'
 import { Button } from './Button'
-import { Input } from './Input'
-
+import { FiCoffee } from 'react-icons/fi'
 interface SearchBarProps {
   onChange?: (
     event: ChangeEvent<HTMLInputElement>
@@ -17,16 +23,31 @@ interface SearchBarProps {
   ) => MouseEventHandler<HTMLButtonElement>
   value?: string
 }
-
+//TODO: integrate dropdown suggestions
 export const SearchBar = (props: SearchBarProps) => {
+  const [focus, setFocus] = useState(false)
   return (
-    <Flex direction="row">
-      <Input
-        type="text"
-        placeholder="Pesquisar startup"
-        value={props.value}
-        onChange={props.onChange}
-      />
+    <Flex direction="row" w="100%">
+      <InputGroup onFocus={() => setFocus(true)} onBlur={() => setFocus(false)}>
+        <InputLeftElement pointerEvents="none" pl="4">
+          <Icon
+            as={FiCoffee}
+            color={focus ? 'brand.500' : 'gray.400'}
+            transition="all 0.15s ease"
+            transform="scaleX(-1)"
+          />
+        </InputLeftElement>
+        <Input
+          pl="12"
+          type="text"
+          placeholder="Nome da startup"
+          value={props.value}
+          onChange={props.onChange}
+          focusBorderColor="none"
+          _placeholder={{ color: 'gray.400' }}
+          bgColor="white"
+        />
+      </InputGroup>
       <Button ml="2" onClick={props.onClick}>
         Pesquisar
       </Button>
