6eded336ad95799727ac93dc5a115e8c4492b80c
guigaribalde <guilherme@creatusdev.com>
2022-06-24 15:19:45 -0300

    style: favicon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import type { AppProps } from 'next/app'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../themes/default'
 import { NavMenu } from '../components/NavMenu'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
       <Box>
         <NavMenu />
         <Component {...pageProps} />
