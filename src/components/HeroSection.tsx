bcf0574fa86f967f34e143298167133d80baf0d9
guigaribalde <guilherme@creatusdev.com>
2022-06-24 15:38:25 -0300

    feat: added HeroSection component base

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,42 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+import { SearchBar } from './SearchBar'
+
+export const HeroSection = () => {
+  return (
+    <Flex
+      pt="24"
+      bgColor="gray.100"
+      w="100%"
+      backgroundImage="/pattern.svg"
+      backgroundPosition="center top"
+    >
+      <Flex
+        mt="4"
+        mx="auto"
+        p="6"
+        pb="12"
+        align="center"
+        justify="center"
+        direction="column"
+        w="800px"
+      >
+        <Text
+          as="h1"
+          fontSize="5xl"
+          fontWeight="bold"
+          textAlign="center"
+          zIndex="1"
+        >
+          Explore Startups
+        </Text>
+        <Text as="h2" textAlign="center" color="gray.600" zIndex="1">
+          A sua startup na palma da sua mão
+        </Text>
+        <Box w="100%" mt="10" zIndex="1">
+          <SearchBar />
+        </Box>
+      </Flex>
+    </Flex>
+  )
+}
