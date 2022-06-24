16224995ab089f9aed78d748cef6893735568557
guigaribalde <guilherme@creatusdev.com>
2022-06-24 11:00:29 -0300

    style: nav z-index and scroll styling

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -5,11 +5,45 @@
 import { Box, Flex, HStack, Text } from '@chakra-ui/react'
 import Logo from '../assets/Logo'
 import { Button } from './Button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const MAX_SCROLL_HEIGHT = 10
 
 export const NavMenu = () => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0)
+  const handleScroll = () => {
+    const position = window.pageYOffset
+    setScrollPosition(position)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <Box position="fixed" w="100%" zIndex="999">
+    <Box
+      position="fixed"
+      w="100%"
+      zIndex="999"
+      bgColor={
+        scrollPosition > MAX_SCROLL_HEIGHT
+          ? 'rgba(237, 242, 247, 0.8)'
+          : 'transparent'
+      }
+      backdropFilter={
+        scrollPosition > MAX_SCROLL_HEIGHT ? 'saturate(1.8) blur(5px)' : 'none'
+      }
+      boxShadow={
+        scrollPosition > MAX_SCROLL_HEIGHT
+          ? 'inset 0 -1px 0 0 rgba(0,0,0,.1);'
+          : 'none'
+      }
+      transition="all 0.3s ease"
+    >
       <Flex
         p="6"
         maxW="1000px"
