4201ec6b5feedc8d136e428919cbb81f87ceaa83
guigaribalde <guilherme@creatusdev.com>
2022-06-24 15:39:20 -0300

    feat: added PromoBox component base

---
+++ src/components/PromoBox.tsx
@@ -0,0 +1,28 @@
+import { Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+interface PromoBoxProps {
+  color: string
+  title: string
+  button?: React.ReactNode
+}
+
+export const PromoBox = (props: PromoBoxProps) => {
+  return (
+    <Flex
+      justify="space-between"
+      direction="column"
+      px="6"
+      py="8"
+      w="360px"
+      h="200px"
+      bgColor={props.color}
+      borderRadius="10"
+    >
+      <Text color="white" fontSize="3xl" fontWeight="semibold">
+        {props.title}
+      </Text>
+      {props.button}
+    </Flex>
+  )
+}
