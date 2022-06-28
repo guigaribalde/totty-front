2d75a2ec2c7cd9f136d91069d6e6dfa237c0465f
guigaribalde <guilherme@creatusdev.com>
2022-06-28 15:55:08 -0300

    feat: added business logo/model component

---
+++ src/components/BusinessLogoAndModel.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Box, Text } from '@chakra-ui/react'
+import { useBusiness } from '@hooks/useBusiness'
+
+export default function BusinessLogoAndModel() {
+  const { business } = useBusiness()
+  return (
+    <Box position="relative">
+      <Avatar
+        name={business.name}
+        src={business.logo}
+        border="1px solid #E2E8F0"
+        size="2xl"
+        bgColor="gray.100"
+        color="gray.400"
+      />
+      <Box
+        position="absolute"
+        bottom="0"
+        right="0"
+        bgColor="brand.500"
+        p="1"
+        px="4"
+        borderRadius="2xl"
+      >
+        <Text color="white" fontWeight="semibold" fontSize="sm">
+          {business.model}
+        </Text>
+      </Box>
+    </Box>
+  )
+}
