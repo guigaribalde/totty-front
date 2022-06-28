ca7078188028a910c34665bcb0bdf9a2ee7ee65d
guigaribalde <guilherme@creatusdev.com>
2022-06-28 18:20:23 -0300

    feat: download page showcase

--- src/components/BusinessProfile/Nav/index.tsx
+++ src/components/BusinessProfile/Nav/index.tsx
@@ -1,9 +1,10 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Flex, Icon, Text } from '@chakra-ui/react'
 
 import BusinessProfileNav from '@components/BusinessProfile/Nav/BusinessProfileNav'
 import AboutCard from '@components/BusinessProfile/Cards/AboutCard'
 import PeopleCard from '@components/BusinessProfile/Cards/PeopleCard'
 import { useState } from 'react'
+import { FiBook, FiPaperclip } from 'react-icons/fi'
 
 export default function BusinessProfileMenu() {
   const [activePage, setActivePage] = useState(0)
@@ -19,7 +20,63 @@ export default function BusinessProfileMenu() {
       {activePage === 1 && (
         <Text p="5">Aqui vão aparecer indicativos financeiros</Text>
       )}
-      {activePage === 2 && <Text p="5">Aqui vão aparecer downloads</Text>}
+      {activePage === 2 && (
+        <Flex
+          direction={{
+            base: 'column',
+            md: 'row',
+          }}
+          mt="2"
+          px="3"
+        >
+          <Box
+            w="100%"
+            borderRadius="md"
+            border="1px solid black"
+            borderColor="gray.200"
+            p="6"
+            mr={{
+              base: '0',
+              md: '2',
+            }}
+            mb={{
+              base: '2',
+              md: '0',
+            }}
+          >
+            <Flex
+              align="center"
+              justify="space-between"
+              direction="column"
+              h="200px"
+              color="gray.700"
+            >
+              <Box />
+              <Icon as={FiBook} w={7} h={7} />
+              <Text fontWeight="semibold">Pitch</Text>
+            </Flex>
+          </Box>
+          <Box
+            w="100%"
+            borderRadius="md"
+            border="1px solid black"
+            borderColor="gray.200"
+            p="6"
+          >
+            <Flex
+              align="center"
+              justify="space-between"
+              direction="column"
+              h="200px"
+              color="gray.700"
+            >
+              <Box />
+              <Icon as={FiPaperclip} w={7} h={7} />
+              <Text fontWeight="semibold">BMC</Text>
+            </Flex>
+          </Box>
+        </Flex>
+      )}
     </Box>
   )
 }
