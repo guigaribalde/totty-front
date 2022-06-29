05b5dfd83554c5a533540d98ebbf2dcb22feaba2
guigaribalde <guilherme@creatusdev.com>
2022-06-29 11:15:23 -0300

    feat: production and development requests

--- src/pages/b/[slug].tsx
+++ src/pages/b/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-// import axios from 'axios'
+import axios from 'axios'
 
 import { Box } from '@chakra-ui/react'
 import Header from '@components/BusinessProfile/Header'
@@ -10,16 +10,22 @@ import { BusinessProvider } from '@context/BusinessContext'
 
 import json from 'src/db/index.json'
 
+const env = process.env.NODE_ENV
+
 export const getServerSideProps: GetServerSideProps = async (props) => {
   const slug = props.query.slug as string
 
-  // const { data: business }: { data: BusinessInterface[] } = await axios.get(
-  //   `http://localhost:3004/business?slug=${slug}`
-  // )
+  const business = []
 
-  const business = [
-    json.business.find((b) => b.slug === slug) as BusinessInterface,
-  ]
+  if (env === 'development') {
+    const { data: b }: { data: BusinessInterface[] } = await axios.get(
+      `http://localhost:3004/business?slug=${slug}`
+    )
+    business.push(b[0])
+  } else if (env === 'production') {
+    const b = json.business.find((b) => b.slug === slug) as BusinessInterface
+    business.push(b)
+  }
 
   return {
     props: { business: business[0] },
