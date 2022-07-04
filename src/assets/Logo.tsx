41a689f9359a3c697cfaf12f2e819f7ca7ed246f
guigaribalde <guilherme@creatusdev.com>
2022-07-04 18:55:03 -0300

    feat: add production and development themes

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -1,5 +1,6 @@
 import { Flex, Text, Image } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+const env = process.env.NODE_ENV
 
 const Logo = () => {
   const router = useRouter()
@@ -9,7 +10,12 @@ const Logo = () => {
 
   return (
     <Flex onClick={handleClick} cursor="pointer">
-      <Image src="/totty.svg" alt="Totty Logo" w={87.933 / 2} h={74.991 / 2} />
+      <Image
+        src={env === 'development' ? '/dev_totty.svg' : '/totty.svg'}
+        alt="Totty Logo"
+        w={87.933 / 2}
+        h={74.991 / 2}
+      />
       <Text fontSize="2xl" fontWeight="semibold" color="brand.500" ml="2">
         Totty
       </Text>
--- src/themes/default.js
+++ src/themes/default.js
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/react'
+const env = process.env.NODE_ENV
 
 export const theme = extendTheme({
   fonts: {
@@ -6,17 +7,34 @@ export const theme = extendTheme({
     subHeading: 'Montserrat, sans-serif',
     body: 'Montserrat, sans-serif',
   },
-  colors: {
-    brand: {
-      100: '#E5D4F5',
-      200: '#CCAAEA',
-      300: '#B280E0',
-      400: '#9856D5',
-      500: '#7F2CCB',
-      600: '#6523A2',
-      700: '#4C1A7A',
-      800: '#331151',
-      900: '#190829',
-    },
-  },
+  colors:
+    env === 'development'
+      ? {
+          brand: {
+            50: '#EDFAF7',
+            100: '#E6F8F4',
+            200: '#CEF1E8',
+            300: '#B5EADD',
+            400: '#9CE3D1',
+            500: '#84DCC6',
+            600: '#71B9AB',
+            700: '#609692',
+            800: '#4E7377',
+            900: '#3C505E',
+          },
+        }
+      : {
+          brand: {
+            50: '#F4EDF7',
+            100: '#E5D4F5',
+            200: '#CCAAEA',
+            300: '#B280E0',
+            400: '#9856D5',
+            500: '#7F2CCB',
+            600: '#6523A2',
+            700: '#4C1A7A',
+            800: '#331151',
+            900: '#190829',
+          },
+        },
 })
