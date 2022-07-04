1008dd44b52a39f75e651c26e24131ab8c61bd5b
guigaribalde <guilherme@creatusdev.com>
2022-07-04 17:24:46 -0300

    feat: added facebook auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import { Box, ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
 
 import { theme } from '@themes/default'
 import { NavMenu } from '@components/NavMenu'
@@ -9,27 +10,29 @@ import { Footer } from '@components/Footer'
 
 import '../themes/index.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter()
   return (
-    <ChakraProvider theme={theme}>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
-      </Head>
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        </Head>
 
-      <Box position="relative" minH="100vh">
-        {router.pathname.includes('/account') ? (
-          <Component {...pageProps} />
-        ) : (
-          <>
-            <NavMenu />
+        <Box position="relative" minH="100vh">
+          {router.pathname.includes('/account') ? (
             <Component {...pageProps} />
-            <Box pt="150px" />
-            <Footer />
-          </>
-        )}
-      </Box>
-    </ChakraProvider>
+          ) : (
+            <>
+              <NavMenu />
+              <Component {...pageProps} />
+              <Box pt="150px" />
+              <Footer />
+            </>
+          )}
+        </Box>
+      </ChakraProvider>
+    </SessionProvider>
   )
 }
 
