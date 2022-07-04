37bf978f5d1c7e22b27b9efd92eb844226423c1f
guigaribalde <guilherme@creatusdev.com>
2022-07-04 18:06:30 -0300

    feat: add linkeding login option

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
+import LinkedInProvider from 'next-auth/providers/linkedin'
 
 export default NextAuth({
   pages: {
@@ -11,6 +12,10 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.SECRET as string,
