54130ddefa5f215db55740bcdda42afd814bd8ea
guigaribalde <guilherme@creatusdev.com>
2022-07-05 15:26:23 -0300

    feat: added prisma Users table

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import { colours } from '@utils/index'
 console.log(colours.fg.yellow, 'PRISMA CLIENT INITIALIZED')
-export const prisma = new PrismaClient()
+const prisma = new PrismaClient()
+export { prisma }
